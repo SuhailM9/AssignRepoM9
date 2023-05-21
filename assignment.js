b488b19c490780fe836df41dd0ac0c162a1949ab
suhail <suhail518650@gmail.com>
2023-05-21 12:17:26 +0530

    screenshot

---
+++ assignment.js
@@ -0,0 +1,3 @@
+let x=45,y=34,a=23,b=3
+let sum=x+y*a-b
+console.log("Total is :",sum)
---
+++ sample.js
@@ -0,0 +1,7 @@
+console.log("hai hellow welcome to js ");
+let x=5
+let y=2
+let result
+console.log("x before :",x)
+x=x+1
+console.log("x after :",x)
