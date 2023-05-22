f5a8985e3288757c8f283b90c0729e7840835b6a
suhail <suhail518650@gmail.com>
2023-05-22 16:11:20 +0530

    fill

---
+++ assignment2-1.js
@@ -0,0 +1,10 @@
+let mark
+let sub1=70
+let sub2=80
+let sub3=90
+let sub4=50
+if (sub1+sub2+sub3+sub4>250) {
+    console.log("Student is Passed");
+}else{
+    console.log("Student is Failed");
+}
---
+++ if.js
@@ -0,0 +1,6 @@
+let i
+let number=3
+for(i=1;i<=10;i++){
+    console.log(i,"*",number,"=",number*i);
+}
+console.log("end");
